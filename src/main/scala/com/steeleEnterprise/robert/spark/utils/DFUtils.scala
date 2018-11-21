dc4fd8006f4bf03dfa333dcb53e102e173f9498d
robertsteele <steele.210@osu.edu>
2018-11-20 21:09:18 -0500

    Initial commit

---
+++ src/main/scala/com/steeleEnterprise/robert/spark/utils/DFUtils.scala
@@ -0,0 +1,10 @@
+package com.steeleEnterprise.robert.spark.utils
+
+import java.util.logging.Logger
+
+object DFUtils extends Serializable {
+
+    @transient lazy val logger = Logger.getLogger(getClass.getName)
+
+    // this is where all dataframe udfs will reside
+}
---
+++ src/main/scala/com/steeleEnterprise/robert/spark/utils/InitSpark.scala
@@ -0,0 +1,32 @@
+package com.steeleEnterprise.robert.spark.utils
+
+import org.apache.log4j.{Level, LogManager, Logger}
+import org.apache.spark.sql.SparkSession
+
+//trait for extending all spark jobs
+
+trait InitSpark {
+
+  val spark: SparkSession = SparkSession.builder()
+    .master("local[*]")
+    .appName("spark-app")
+    .enableHiveSupport()
+    .getOrCreate()
+
+  val sc = spark.sparkContext
+
+  val sql = spark.sqlContext
+
+  private def init(): Unit = {
+    sc.setLogLevel("ERROR")
+    Logger.getLogger("org").setLevel(Level.ERROR)
+    Logger.getLogger("akka").setLevel(Level.ERROR)
+    LogManager.getRootLogger.setLevel(Level.ERROR)
+  }
+
+  def close(): Unit = {
+    spark.close()
+  }
+
+  init()
+}
